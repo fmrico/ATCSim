69f09567eb73b0e18eaf1f244c67f0199f2abf84
Pablo Asensio Martinez <pasensio@alpha28.aulas.gsyc.urjc.es>
2018-11-26 14:00:10 +0100

    xxx

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/alumnos/pasensio/Escritorio/universidad/programacion/cpp/git/ATCSim
+# Build directory: /home/alumnos/pasensio/Escritorio/universidad/programacion/cpp/git/ATCSim/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(Position_test "Position_test")
+set_tests_properties(Position_test PROPERTIES  TIMEOUT "5")
