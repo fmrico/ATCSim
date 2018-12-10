85628ed1c1b3b599fe47986f7ec9e4682612eea4
antoniSepu <antonicriptor@gmail.com>
2018-12-10 12:37:14 +0100

    Actualizado ok con cpplint

--- src/lib/Common.h
+++ src/lib/Common.h
@@ -23,70 +23,69 @@
  */
 
 
-#ifndef ATCSIM_LIB_COMMON_H_
-#define ATCSIM_LIB_COMMON_H_
+#ifndef SRC_LIB_COMMON_H_
+#define SRC_LIB_COMMON_H_
 
+#include <stdlib.h>
 #include <cmath>
 #include <cstdlib>
 #include <string>
 
-#include <stdlib.h>
-
 #define COLLISION_DISTANCE 1000.0f
 #define CRASH_Z 20.0f
 #define CRASH_INC 25.0f
-#define CRASH_SPEED_MAX 265.0f   // Approx. 950 km/h
+#define CRASH_SPEED_MAX 265.0f  // Approx. 950 km/h
 #define CRASH_SPEED 10.0f
 #define INIT_MAX_FLIGHTS 1
 
-#define MAX_FLIFGT_W	0.2f //12 grados/sec
-#define DIST_POINT 	200.0 //15 grados/sec
-#define MAX_ACELERATION 	50.0 //50 m/sec2
+#define MAX_FLIFGT_W 0.2f  // 12 grados/sec
+#define DIST_POINT 200.0  // 15 grados/sec
+#define MAX_ACELERATION 50.0  // 50 m/sec2
 
 
-#define AIRPORT_DISTANCE_MAX 	20000.0f
-#define FLIGHT_HEIGHT	 		2000.0f
-#define LANDING_STRIP_L	 		750.0f
-#define LANDING_STRIP_W	 		80.0f
+#define AIRPORT_DISTANCE_MAX 20000.0f
+#define FLIGHT_HEIGHT 2000.0f
+#define LANDING_STRIP_L 750.0f
+#define LANDING_STRIP_W 80.0f
 
-#define STORM_MAX_RAD				5000.0f
-#define STORM_MIN_RAD				2000.0f
-#define STORM_MAX_HEIGHT				2000.0f
-#define STORM_MIN_HEIGHT				500.0f
-#define STORM_MAX_ELEVATION				2000.0f
-#define STORM_MIN_ELEVATION				500.0f
-#define STORM_MAX_SPEED				500.0f
-#define STORM_MIN_SPEED				30.0f
-
-
-//Conditions for landing
-
-#define LANDING_POS_X		0.0f
-#define LANDING_POS_Y		0.0f
-#define LANDING_POS_Z		25.0f
+#define STORM_MAX_RAD 5000.0f
+#define STORM_MIN_RAD 2000.0f
+#define STORM_MAX_HEIGHT 2000.0f
+#define STORM_MIN_HEIGHT 500.0f
+#define STORM_MAX_ELEVATION 2000.0f
+#define STORM_MIN_ELEVATION 500.0f
+#define STORM_MAX_SPEED 500.0f
+#define STORM_MIN_SPEED 30.0f
 
 
-#define LANDING_SPEED 			20.0f
-#define LANDING_BEAR  			-180.0f
-#define LANDING_BEAR_MAX_ERROR	20.0f
-#define LANDING_DIST  			50.0f
+// Conditions for landing
 
-#define INIT_FLIGHT_POINTS  200
+#define LANDING_POS_X 0.0f
+#define LANDING_POS_Y 0.0f
+#define LANDING_POS_Z 25.0f
+
+
+#define LANDING_SPEED 20.0f
+#define LANDING_BEAR -180.0f
+#define LANDING_BEAR_MAX_ERROR 20.0f
+#define LANDING_DIST 50.0f
+
+#define INIT_FLIGHT_POINTS 200
 #define INIT_POINTS 0
-#define LANDING_POINTS 	 	(10)
-#define COLLISION_POINTS 	(-50)
-#define CRASH_HEIGHT_POINTS	(-25)
-#define CRASH_INC_POINTS	(-25)
-#define CRASH_SPEED_POINTS 	(-25)
+#define LANDING_POINTS (10)
+#define COLLISION_POINTS (-50)
+#define CRASH_HEIGHT_POINTS (-25)
+#define CRASH_INC_POINTS (-25)
+#define CRASH_SPEED_POINTS (-25)
 
-#define INC_DIFF		30000000//30 sec
-#define INC_PEN			1 	 	//fligts/inc
+#define INC_DIFF 30000000  // 30 sec
+#define INC_PEN 1  // fligts/inc
 
-#define INC_SIMTIME			0.1
+#define INC_SIMTIME 0.1
 
-inline double sec(const double a){return 1/cos(a);}
+inline double sec(const double a) {return 1/cos(a);}
 
-inline double cosec(const double a){return 1/sin(a);}
+inline double cosec(const double a) {return 1/sin(a);}
 
 /** @name constants for some often used angles */
 ///@{
@@ -114,14 +113,14 @@ const double sqrt2pi = sqrt(2.0*pi);
  * \param angle code in rad
  * \return angle coded in degrees
  */
-inline double toDegrees(double angle){return angle*(180.0/pi);}
+inline double toDegrees(double angle) {return angle * (180.0 / pi);}
 
 /** Converts angle from degrees to rad.
  * \param degrees angle coded in degrees
  * \return angle coded in rad
  */
 
-inline double toRadians(double degrees){return degrees*pi_180;}
+inline double toRadians(double degrees) {return degrees * pi_180;}
 
 /**
 * reduce angle to [-pi..+pi[
@@ -129,13 +128,16 @@ inline double toRadians(double degrees){return degrees*pi_180;}
 * \return normalized angle coded in rad
 */
 
-inline double normalizePi(double data)
-{
+inline double normalizePi(double data) {
   if (data < pi && data >= -pi) return data;
   double ndata = data - ((int )(data / pi2))*pi2;
-  while (ndata >= pi) { ndata -= pi2; };
+  while (ndata >= pi) {
+    ndata -= pi2;
+  }
 
-  while (ndata < -pi) { ndata += pi2; };
+  while (ndata < -pi) {
+    ndata += pi2;
+  }
 
   return ndata;
 }
@@ -144,14 +146,14 @@ inline double normalizePi(double data)
 * The function returns a random number in the range of [0..1].
 * @return The random number.
 */
-inline double randomDouble() {return double(rand()) / RAND_MAX;}
+inline double randomDouble() {return static_cast<double>(rand_r())/RAND_MAX;}
 
 /**
 * The function returns a random integer number in the range of [0..n-1].
 * @param n the number of possible return values (0 ... n-1)
 * @return The random number.
 */
-inline int random(int n) {return (int)(randomDouble()*n*0.999999);}
+inline int random(int n) {return static_cast<int>(randomDouble()*n*0.999999);}
 
 /** constant, cast before execution*/
 const double RAND_MAX_DOUBLE = static_cast<double>(RAND_MAX);
@@ -161,9 +163,8 @@ const double RAND_MAX_DOUBLE = static_cast<double>(RAND_MAX);
 * @param n the number of possible return values (0 ... n)
 * @return The random number.
 */
-inline int randomFast(int n)
-{
-  return static_cast<int>((rand() * n) / RAND_MAX_DOUBLE);
+inline int randomFast(int n) {
+  return static_cast<int>((rand_r() * n) / RAND_MAX_DOUBLE);
 }
 
 /**
@@ -171,37 +172,35 @@ inline int randomFast(int n)
 * @param d A number
 * @return The number as integer
 */
-inline int roundNumberToInt(double d) { return static_cast<int> (round(d)); }
+inline int roundNumberToInt(double d) {return static_cast<int> (round(d));}
 
 /**
 * Round to the next integer but keep type
 * @param d A number
 * @return The number
 */
-inline double roundNumber(double d) { return round(d); }
+inline double roundNumber(double d) {return round(d);}
 
-inline float normalAng(float x, float mu, float st)
-{
-	float diff = normalizePi(x-mu);
-	float d1 = st*sqrt(2.0*pi);
-	float d2 = 2*st*st;
+inline float normalAng(float x, float mu, float st) {
+  float diff = normalizePi(x-mu);
+  float d1 = st*sqrt(2.0*pi);
+  float d2 = 2*st*st;
 
-	if (d1==0.0) d1 = 0.00001;
-	if (d2==0.0) d2 = 0.00001;
+  if (d1 == 0.0) d1 = 0.00001;
+  if (d2 == 0.0) d2 = 0.00001;
 
 	return (1.0/d1) * exp(-(diff * diff)/d2);
 }
 
-inline float normalDist(float x, float mu, float st)
-{
+inline float normalDist(float x, float mu, float st) {
 	float diff = x-mu;
 	float d1 = st*sqrt(2.0*pi);
 	float d2 = 2*st*st;
 
-	if (d1==0.0) d1 = 0.00001;
-	if (d2==0.0) d2 = 0.00001;
+  if (d1 == 0.0) d1 = 0.00001;
+	if (d2 == 0.0) d2 = 0.00001;
 
 	return (1.0/d1)*exp(-(diff*diff)/d2);
 }
 
-#endif // ATCSIM_LIB_COMMON_H_
+#endif  // SRC_LIB_COMMON_H_
