8c20ecfb9a5fe78aa7f9d38a92082e96bbaa16c7
Pablo Asensio <p.asensio97@gmail.com>
2018-12-13 20:47:10 +0100

    Pantalla en negro y gtes deshabilitados en APPLE, ubuntu ok

--- build_ubuntu_18_04/googletest-prefix/src/googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pablo/git/ATCSim/build_ubuntu_18_04/googletest-prefix/src/googletest
-# Build directory: /home/pablo/git/ATCSim/build_ubuntu_18_04/googletest-prefix/src/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- build_ubuntu_18_04/googletest-prefix/src/googletest-build/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pablo/git/ATCSim/build_ubuntu_18_04/googletest-prefix/src/googletest/googletest
-# Build directory: /home/pablo/git/ATCSim/build_ubuntu_18_04/googletest-prefix/src/googletest-build/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
