ad5bb052404d1d2db6cc7da3862e8ef33a259648
Pablo Asensio <p.asensio97@gmail.com>
2018-12-13 20:46:23 +0100

    Pantalla en negro y gtes deshabilitados en APPLE, ubuntu ok

--- build_MacOS/CMakeFiles/cppgui_cpplint.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/cppgui_cpplint"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/cppgui_cpplint.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
