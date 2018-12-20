7d4f7216fc47ff588f96aa852ba1b776293e25a4
Jaime Valle <j.valleal@alumnos.urjc.es>
2018-12-20 22:05:12 +0100

    NEW: Añadido test para la clase CircuitRoute.

---
+++ test/CircuitRoute_test.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "gtest/gtest.h"
+#include "CircuitRoute.h"
+
+
+TEST(CircuitRouteTest, loadNavPoints)
+{
+    int error_code = atcsim::loadNavPoints();
+
+    if(error_code == EXIT_SUCCESS)
+        ASSERT_GE(atcsim::getRoutePoints().size(), 0);
+    else
+        ASSERT_EQ(atcsim::getRoutePoints().size(), 0);
+}
+
+TEST(CircuitRouteTest, loadNavCircuits)
+{
+    int error_code = atcsim::loadNavCircuits();
+
+    if(error_code == EXIT_SUCCESS)
+        ASSERT_GE(atcsim::getRouteCircuits().size(), 0);
+    else
+        ASSERT_EQ(atcsim::getRouteCircuits().size(), 0);
+}
+
+
+int main(int argc, char* argv[])
+{
+  ::testing::InitGoogleTest(&argc, argv);
+
+  return RUN_ALL_TESTS();
+}
